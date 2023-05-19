aa622bd94d19381487de85e216ba2b7738eed8c3
Md Rabi Ul Uslam <58680120+rabiulislam007@users.noreply.github.com>
2023-05-19 16:42:58 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,16 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>This is Home Page</div>
+    <div>
+      <div>
+        /////// NavBar
+      </div>
+      <div>
+        /////// Main Content
+      </div>
+      <div>
+        ////// Footer
+      </div>
+    </div>
   )
 }
